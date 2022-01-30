88b29206f9dc12f54b3329455a5e9d855378846b
unknown <anibalcatalanf@gmail.com>
2022-01-29 21:59:25 -0300

    tast

---
+++ test/types.ts
@@ -0,0 +1,16 @@
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import type { Fixture } from "ethereum-waffle";
+
+import type { Vesting } from "../src/types/Vesting";
+
+declare module "mocha" {
+    export interface Context {
+        vesting: Vesting;
+        loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
+        signers: Signers;
+    }
+}
+
+export interface Signers {
+    admin: SignerWithAddress;
+}
